ad6f09536d363a0c1cd22293faa0b8961054bc39
Kayley Clark <you@example.com>
2022-04-14 12:55:44 -0600

    first commit

---
+++ Week3 coding assignment.js
@@ -0,0 +1,129 @@
+//1
+//var ages = [3, 9, 23, 64, 2, 8, 28, 93]
+
+//var lastElement = ages[ages.length-1];
+//var first = ages[0]
+//console.log(lastElement - first);
+
+//ages.push(6);
+//lastElement = ages[ages.length-1]
+//console.log(lastElement - first);
+
+//var sum = 0;
+//for (let i = 0; i < ages.length; i++){
+//    sum = sum + ages[i] 
+//}
+// console.log(sum/ages.length)
+
+//2
+//const names = ['Sam', 'Tommy','Tim','Sally','Buck','Bob'];
+
+
+//namesCharCount = names.length;
+//console.log(namesCharCount)
+//joinedNames = names.join('')
+//console.log(joinedNames)
+//resultOfNameLenght = joinedNames.length/namesCharCount;
+//console.log(resultOfNameLenght)
+
+
+//const lastName = names[names.length-1]
+//console.log(lastName);
+
+
+//const firstName = names[0]
+//console.log(firstName);
+
+//3
+//const lastItem = names[names.length-1];
+
+//4
+//const firstName = names[0];
+
+//5
+//let nameLengths = [];
+
+//for(var i=0;i<names.length;i++)
+//nameLengths.push(names[i].length); 
+
+//console.log('names: ',names);
+//console.log('nameLengths: ',nameLengths);
+
+//6 
+//let totalLengths = 0;
+
+//for(var i=0;i<nameLengths.length;i++)
+//  totalLengths += nameLengths[i];
+
+//console.log('Sum of all elements: ',totalLengths);
+
+
+//7
+//func = (word, n) => {
+
+  //  var concat = ""
+    
+  // while (n != 0) {
+   // concat += word 
+   // n--  
+// }
+  //  return concat
+    // }
+   // console.log(func("Hello", 3))
+
+//8
+//function createFullName(firstName, lastName){
+//    console.log(firstName + " " + lastName);
+//}
+//createFullName("Kayley", "Clark");
+
+//9
+
+//10
+//function average(arr){    
+  //  var total = 0;
+   // for(var i = 0; i < arr.length; i++) {
+    //    total += arr[i];
+   // }
+    //var avg = total / arr.length;
+    //return avg;
+   // }
+    
+    //var av=average([1,2,3,4,5,6]);
+    //console.log(av);
+
+  //11
+  //function isGreaterAvg(arr1,arr2){    
+    //var total1 = 0;
+   // for(var i = 0; i < arr1.length; i++) {
+   //     total1 += arr1[i];
+  //  }
+  //  var total2=0;
+   // for(var i = 0; i < arr2.length; i++) {
+   //     total2 += arr2[i];
+  //  }
+   // var avg1 = total1 / arr1.length;
+   // var avg2 = total2 / arr2.length;
+   // if (avg1>avg2){
+   //     return true;
+   // }
+   // else{
+   //     return false;
+   // }
+   // }
+    
+    //var a=isGreaterAvg([6,7,8,9,10],[1,2,3,4,5]);
+   // console.log(a);
+   
+   //12
+   //function willBuyDrink(isHotOutside,moneyInPocket){  
+    //if(isHotOutside && moneyInPocket>10.5){
+    //    return true;
+   // }
+   // else{
+    //    return false;
+    //}
+//}
+//var a=willBuyDrink(true,12);
+//console.log(a);
+
